39558f8220fdde1762131250d6950a9d567449c2
Krishjain <krish7jain@gmail.com>
2023-01-20 09:58:36 +0530

    Update SYM.py
    Removing default return type

--- HW1/SYM.py
+++ HW1/SYM.py
@@ -5,23 +5,23 @@ from math import log
 
 class SYM:
 
-    def __init__(self) -> None: # Constructor
+    def __init__(self): # Constructor
         self.n   = 0
         self.has = {}
         self.most, = 0
         self.mode = None
 
-    def add(self, x) -> None: # update counts of things seen so far         
+    def add(self, x): # update counts of things seen so far         
         if x != '?':
             self.n += 1
             self.has[x] = 1 + (0 if not self.has.get(x) else self.has.get(x))
             if self.has[x] > self.most:
                 self.most, self.mode = self.has[x], x
     
-    def mid(self) -> int: # return the mode
+    def mid(self): # return the mode
         return self.mode
     
-    def div(self, fun) -> int: # return the entropy
+    def div(self, fun): # return the entropy
         def fun(p): return p*log(p, 2)
         e = 0
         for _, n in self.has.items():
