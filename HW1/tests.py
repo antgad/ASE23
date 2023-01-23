c5126755557b8a84702c02b3362680e8d0cb9902
antgad <anantgadodia.17@gmail.com>
2023-01-23 15:51:01 -0500

    Update tests.py

--- HW1/tests.py
+++ HW1/tests.py
@@ -9,10 +9,19 @@ def test_sym():
     assert sym.mid=="a" and utils.rand(sym.div())==1.379
 
 def test_num():
-    sym=SYM()
+    num=NUM()
     for x in [1,1,1,1,2,2,3]:
-        sym.add(x)
-    assert sym.mid==11/7 and utils.rnd(sym.div())==0.787 
+        num.add(x)
+    assert num.mid==11/7 and utils.rnd(num.div())==0.787 
+def test_rand():
+    num1=NUM()
+    num2=NUM()
+    for i in range(1,10^3):
+        num1.add(utils.rand(0,1))
+    for i in range(1,10^3):
+        num2.add(utils.rand(0,1))
+    m1=utils.rnd(num1.mid(),10)
+    m2=utils.rnd(num2.mid(),10)
+    assert m1==m2 and utils.rnd(m1,1)==0.5
 
 
-    
