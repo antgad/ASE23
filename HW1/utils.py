8d3512934fad7ddb2afcf5308252db70c9775699
Anant Gadodia <37169203+antgad@users.noreply.github.com>
2023-01-23 15:44:33 -0500

    Update utils.py

--- HW1/utils.py
+++ HW1/utils.py
@@ -8,9 +8,7 @@ def rand(lo=0,hi=1):
     Seed = (16807 * Seed) % 2147483647
     return lo + (hi-lo) * Seed / 2147483647 
 
-function rnd(n, nPlaces) --> num. return `n` rounded to `nPlaces`
-  local mult = 10^(nPlaces or 3)
-  return math.floor(n * mult + 0.5) / mult
+
 
 def rnd(n,nPlaces=3):
     mult = pow(10,nPlaces)
