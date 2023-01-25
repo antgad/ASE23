2327f083db6ac936fdbc4d7349461499e80ae464
Krish Jain <krishjain@Krishs-MacBook-Air.local>
2023-01-25 15:56:51 +0530

    Updating __init__ to add at,txt and w

--- HW2/NUM.py
+++ HW2/NUM.py
@@ -5,9 +5,11 @@ from math import inf
 
 class NUM:
 
-    def __init__(self):
+    def __init__(self, at, txt):
+        self.at = at or 0, self.txt = txt or ""
         self.n, self.mu, self.m2 = 0, 0, 0
         self.lo, self.hi = inf, -inf
+        self.w = -1 if "-" in self.txt else 1
     
     def add(self, n): # add `n`, update lo,hi and stuff needed for standard deviation
         if n != '?':
@@ -27,3 +29,26 @@ class NUM:
         else:
             return (self.m2/(self.n-1))**0.5
     
+function NUM.new(i,at,txt) --> NUM;  constructor; 
+  i.at, i.txt = at or 0, txt or "" -- column position and name
+  i.n, i.mu, i.m2 = 0, 0, 0
+  i.lo, i.hi = math.huge, -math.huge 
+  i.w = i.txt:find"-$" and -1 or 1 end
+
+function NUM.add(i,n,    d) --> NUM; add `n`, update lo,hi and stuff needed for standard deviation
+  if n ~= "?" then
+    i.n  = i.n + 1
+    d = n - i.mu
+    i.mu = i.mu + d/i.n
+    i.m2 = i.m2 + d*(n - i.mu)
+    i.lo = math.min(n, i.lo)
+    i.hi = math.max(n, i.hi) end end
+
+
+if "-" in self.txt:
+            self.w = -1
+        else:
+            self.w = 1
+
+i.w = -1 if i.txt.find("-") != -1 else 1
+
