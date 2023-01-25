82f984983ede535c356db6adbcec28230c5d12f9
Krish Jain <krishjain@Krishs-MacBook-Air.local>
2023-01-25 15:43:26 +0530

    adding COLS class functionality in cols.py

---
+++ HW2/COLS.py
@@ -0,0 +1,34 @@
+import re
+
+from num import NUM
+from sym import SYM
+from row import ROW
+
+class COLS:
+    """
+        Factory for managing a set of NUMs or SYMs
+    """
+    def __init__(self,t):
+        self.names = t
+        self.all = []
+        self.x = []
+        self.y = []
+        self.klass = None    
+
+        for n, s in enumerate(t):
+            col =  NUM(n, s) if re.findall(r"^[A-Z]+",s) else SYM(n, s)
+            self.all.append(col)
+
+            if not re.findall(r"X$", s):
+                if re.findall(r"!$", s):
+                    self.klass = col
+                if re.findall(r"[!+-]$", s):
+                    self.y.append(col)
+                else:
+                    self.x.append(col)
+
+    def add(self, row):
+        for _, t in enumerate({self.x, self.y}):
+            for _, col in enumerate(t):
+                col.add(row.cells[col.at])
+                
