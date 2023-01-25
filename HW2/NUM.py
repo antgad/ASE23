b333b877bec1fc6b881ce8d5b4be5b6df7abf696
Krish Jain <krishjain@Krishs-MacBook-Air.local>
2023-01-25 16:14:39 +0530

    Modifying __init__ to add at, txt and w

--- HW2/NUM.py
+++ HW2/NUM.py
@@ -28,27 +28,3 @@ class NUM:
             return 0
         else:
             return (self.m2/(self.n-1))**0.5
-    
-function NUM.new(i,at,txt) --> NUM;  constructor; 
-  i.at, i.txt = at or 0, txt or "" -- column position and name
-  i.n, i.mu, i.m2 = 0, 0, 0
-  i.lo, i.hi = math.huge, -math.huge 
-  i.w = i.txt:find"-$" and -1 or 1 end
-
-function NUM.add(i,n,    d) --> NUM; add `n`, update lo,hi and stuff needed for standard deviation
-  if n ~= "?" then
-    i.n  = i.n + 1
-    d = n - i.mu
-    i.mu = i.mu + d/i.n
-    i.m2 = i.m2 + d*(n - i.mu)
-    i.lo = math.min(n, i.lo)
-    i.hi = math.max(n, i.hi) end end
-
-
-if "-" in self.txt:
-            self.w = -1
-        else:
-            self.w = 1
-
-i.w = -1 if i.txt.find("-") != -1 else 1
-
