2d0afafec131a4bf1fc89424785c57a7c1acb8db
anantgadodia <anantgadodia.17@gmail.com>
2023-03-23 18:55:14 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -50,10 +50,10 @@ def rint(lo,hi):
     x, Seed= rand(lo, hi)
     return math.floor(0.5 + x)
 
-def rand(lo=0,hi=1, Seed=937162211):
+def rand(lo=0,hi=1):
     # global Seed
     Seed = (16807 * Seed) % 2147483647
-    return lo + (hi-lo) * Seed / 2147483647, Seed  
+    return lo + (hi-lo) * Seed / 2147483647  
 
 def rnd(n,nPlaces=2):
     mult = pow(10,nPlaces)
