3c3d935e6887916b4a27f4cfd114b936a0d3c21a
antgad <anantgadodia.17@gmail.com>
2023-03-24 14:12:19 -0400

    Update main_hw6.py

--- src/main_hw6.py
+++ src/main_hw6.py
@@ -162,6 +162,7 @@ def test_cliffs():
     while (not diff):
         def function(x):
             return x*j
+        print(t1)
         t3=list(map(function, t1))
         rx = cliffsDelta(t1,t3) > options['cliffs']
         diff= rx
